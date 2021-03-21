6e1fadd67d4cbc7703b64e522fa8725a897ebb60
shivatinker <azinoviev@readdle.com>
2021-03-21 17:57:15 +0200

    Initial Commit

---
+++ ONUGraphics/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ONUGraphics
+//
+//  Created by Andrii Zinoviev on 21.03.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
